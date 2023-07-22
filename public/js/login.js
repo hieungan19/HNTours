b150eb9c41e198b75101561d9ebfbafa6c3c47be
Hieu Ngan <21520358@gm.uit.edu.vn>
2023-07-22 09:24:09 +0700

    fix bug when log in

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,5 @@
 const login = async (email, password) => {
-  fetch('http://localhost:3000/api/v1/users/login', {
+  fetch('/api/v1/users/login', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -11,7 +11,7 @@ const login = async (email, password) => {
   })
     .then((response) => response.json())
     .then((data) => {
-      //console.log(data);
+      console.log(data);
       if (data.status === 'success') {
         alert('Logged in successfully.');
         window.setTimeout(() => {
@@ -22,7 +22,7 @@ const login = async (email, password) => {
       }
     })
     .catch((error) => {
-      //console.log(error);
+      console.log(error);
     });
 };
 document.querySelector('.form--login').addEventListener('submit', (e) => {
